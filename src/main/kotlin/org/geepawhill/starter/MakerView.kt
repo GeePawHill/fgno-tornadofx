5e1476e31173df5652ce9168e43586bcba0beb04
GeePaw Hill <geepawhill@geepawhill.org>
2022-08-02 10:50:14 -0400

    Messing with graphics, and got css warning cleared.

--- src/main/kotlin/org/geepawhill/starter/MakerView.kt
+++ src/main/kotlin/org/geepawhill/starter/MakerView.kt
@@ -1,18 +1,21 @@
 package org.geepawhill.starter
 
+import javafx.event.EventHandler
 import javafx.scene.image.Image
-import javafx.scene.image.ImageView
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyEvent
+import javafx.scene.input.MouseEvent
 import javafx.scene.layout.Background
 import javafx.scene.layout.BackgroundFill
 import javafx.scene.paint.Color
 import tornadofx.*
 
 const val SCREEN_WIDTH = 800.0
-const val SCREEN_HALF = SCREEN_WIDTH/2.0
+const val SCREEN_HALF = SCREEN_WIDTH / 2.0
 const val SCREEN_HEIGHT = 480.0
 const val BUCKET_WIDTH = 64.0
 const val BUCKET_HEIGHT = 64.0
-const val BUCKET_HALF = BUCKET_WIDTH/2.0
+const val BUCKET_HALF = BUCKET_WIDTH / 2.0
 
 class MakerView : View("Raindrops") {
 
@@ -22,12 +25,43 @@ class MakerView : View("Raindrops") {
         image = Image("/bucket.png")
     }
 
+    val keyHandler = EventHandler<KeyEvent> {
+        handleKey(it)
+    }
+
     override val root = pane {
         minWidth = SCREEN_WIDTH
         maxWidth = SCREEN_WIDTH
         minHeight = SCREEN_HEIGHT
         maxHeight = SCREEN_HEIGHT
-        background = Background(BackgroundFill(Color.LIGHTBLUE,null,null))
-        this+=bucket
+        background = Background(BackgroundFill(Color.LIGHTBLUE, null, null))
+        isFocusTraversable = true
+        addEventHandler(KeyEvent.KEY_PRESSED) { e: KeyEvent ->
+            handleKey(e)
+        }
+        addEventHandler(MouseEvent.MOUSE_CLICKED) { e: MouseEvent ->
+            requestFocus()
+        }
+        this += bucket
+    }
+
+    init {
+        root.requestFocus()
+    }
+
+    private fun handleKey(event: KeyEvent) {
+        when (event.code) {
+            KeyCode.A, KeyCode.LEFT -> left()
+            KeyCode.D, KeyCode.RIGHT -> right()
+            else -> {}
+        }
+    }
+
+    private fun left() {
+        println("l")
+    }
+
+    private fun right() {
+        println("r")
     }
 }
